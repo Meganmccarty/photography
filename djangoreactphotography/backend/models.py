080a743404ce58ae9d391b30b10733ee0515d224
Megan McCarty <megan_mccarty@hotmail.com>
2020-10-04 14:30:52 -0400

    Added photo model

--- djangoreactphotography/backend/models.py
+++ djangoreactphotography/backend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from sorl.thumbnail import ImageField
 
-# Create your models here.
+
+class Photo(models.Model):
+
+    image = models.ImageField(upload_to="photos/")
+    name = models.CharField(max_length=1000, null=True, blank=True)
+    caption = models.TextField(max_length=2000, null=True, blank=True)
+    date_taken = models.DateField(null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
