498d2c68847d8564d01b965684202c01fb1b0bf2
Megan McCarty <megan_mccarty@hotmail.com>
2020-10-15 14:11:34 -0400

    Refactored nav element

--- djangoreactphotography/frontend/src/components/Navbar.js
+++ djangoreactphotography/frontend/src/components/Navbar.js
@@ -2,6 +2,28 @@ import React from 'react';
 import logo from '../images/logo-white.png';
 
 
+class Menu extends React.Component {
+    render() {
+        return (
+            <ul>
+                <a href="#"><li>Butterflies</li></a>
+                <a href="#"><li>Arthropods</li></a>
+                <a href="#"><li>Animals</li></a>
+                <a href="#"><li>Pets</li></a>
+                <a href="#"><li>Plants and Flowers</li></a>
+                <a href="#"><li>Landscapes and Nature</li></a>
+                <a href="#"><li>Sunsets</li></a>
+                <a href="#"><li>Weather</li></a>
+                <a href="#"><li>Spring</li></a>
+                <a href="#"><li>Autumn</li></a>
+                <a href="#"><li>Winter</li></a>
+                <a href="#"><li>Buildings and Architecture</li></a>
+                <a href="#"><li>Other</li></a>
+            </ul>
+        )
+    }
+}
+
 class Toggle extends React.Component {
     constructor(props) {
         super(props);
@@ -21,29 +43,13 @@ class Toggle extends React.Component {
                 &#9776; Menu
                 <div id="nav" className={this.state.isMenuClosed ? 'closed' : 'open'}>
                     <nav>
-                        <ul>
-                            <a href="#"><li>Butterflies</li></a>
-                            <a href="#"><li>Arthropods</li></a>
-                            <a href="#"><li>Animals</li></a>
-                            <a href="#"><li>Pets</li></a>
-                            <a href="#"><li>Plants and Flowers</li></a>
-                            <a href="#"><li>Landscapes and Nature</li></a>
-                            <a href="#"><li>Sunsets</li></a>
-                            <a href="#"><li>Weather</li></a>
-                            <a href="#"><li>Spring</li></a>
-                            <a href="#"><li>Autumn</li></a>
-                            <a href="#"><li>Winter</li></a>
-                            <a href="#"><li>Buildings and Architecture</li></a>
-                            <a href="#"><li>Other</li></a>
-                        </ul>
+                        <Menu />
                     </nav>
                 </div> 
             </span> 
         );
     }
 }
-  
-
 
 function Navbar() {
     return (
@@ -52,21 +58,7 @@ function Navbar() {
             <h1>Megan McCarty Photography</h1>
             <Toggle />
             <nav id="nav-lg">
-                <ul>
-                    <a href="#"><li>Butterflies</li></a>
-                    <a href="#"><li>Arthropods</li></a>
-                    <a href="#"><li>Animals</li></a>
-                    <a href="#"><li>Pets</li></a>
-                    <a href="#"><li>Plants and Flowers</li></a>
-                    <a href="#"><li>Landscapes and Nature</li></a>
-                    <a href="#"><li>Sunsets</li></a>
-                    <a href="#"><li>Weather</li></a>
-                    <a href="#"><li>Spring</li></a>
-                    <a href="#"><li>Autumn</li></a>
-                    <a href="#"><li>Winter</li></a>
-                    <a href="#"><li>Buildings and Architecture</li></a>
-                    <a href="#"><li>Other</li></a>
-                </ul>
+                <Menu />
             </nav>
         </div>
     )
