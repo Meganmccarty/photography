04540a629fe306abff31cae6427dfff322912ca4
Megan McCarty <megan_mccarty@hotmail.com>
2020-10-20 13:17:34 -0400

    Connected frontend with backend

--- djangoreactphotography/frontend/src/index.js
+++ djangoreactphotography/frontend/src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import axios from 'axios';
 import './styles.css';
 import * as serviceWorker from './serviceWorker';
+
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.withCredentials = true;
+
+axios.post('/login/', { username: 'meganmccarty', password: 'B@77u$ph!13n0r'}).then(rv => {
+    console.log('Login', rv)
+    /*axios.get('/api/photos/').then(resp => {
+        console.log('Response', resp)
+    }).catch(err => {
+        console.log('Error', err.response.status)
+    });*/
+}).catch(err => {
+    console.log('Login error', err.response)
+});
+
+const updatePhoto = () => {
+    axios.patch('http://localhost:8000/api/photos/').then(resp => {
+        console.log('Update response', resp)
+    }).catch(error => {
+        console.log("Update error", error)
+    })
+}
+
+/*axios.get('/api/').then(resp => {
+    console.log('Response', resp)
+}).catch(err => {
+    console.log('Error', err.response.status)
+});*/
 
 ReactDOM.render(
   <React.StrictMode>
