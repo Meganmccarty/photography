a2638532d3d48ee0ef57314f5e8be39cfd83dbc4
Megan McCarty <megan_mccarty@hotmail.com>
2020-12-08 10:37:44 -0500

    Added stopStealPhoto.js from https://github.com/andymarch25/stopStealPhoto

--- djangoreactphotography/frontend/src/components/App.js
+++ djangoreactphotography/frontend/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 import Navbar from './Navbar.js';
 import Gallery from './Gallery.js';
 import About from './About.js';
@@ -24,5 +25,78 @@ class App extends React.Component {
         )
     }
 }
+
+/* Below is stopStealPhoto.js code to disable right-clicking photos 
+and replace with copyright notice; code taken from Andrew Novikov (andymarch25):
+https://github.com/andymarch25/stopStealPhoto */
+
+$.fn.stopStealPhoto = function(options) {
+  
+    // default settings
+    var settings = $.extend({
+        message: 'Image © Megan McCarty',
+        delay: 600,
+        customClass: undefined,
+        cursorOffset: 14
+    }, options);
+
+    var timer;
+    var elem = this;
+
+    $('body').on('contextmenu', 'img', function (event) {
+        event.preventDefault();
+
+        var currentElement = event.currentTarget;
+
+        var hintPositionX = event.clientX + settings.cursorOffset;
+        var hintPositionY = event.clientY + settings.cursorOffset;
+
+        window.clearTimeout(timer);
+        $('.ssph-hint').remove();
+
+        var newId = Math.floor(Date.now());
+
+        var hint = $('<div class="ssph-hint" id="ssph-'+newId+'" />');
+
+        hint.css({
+             'position' : 'absolute',
+             'left' : hintPositionX,
+             'top' : hintPositionY,
+             'z-index' : 100
+        })
+
+        if (settings.customClass) {
+             
+            hint.addClass(settings.customClass)
+
+        } else {
+            hint.css({
+                'background-color': 'rgba(	73, 138, 104,0.75)',
+                'color': '#fff',
+                'font-size': '11px',
+                'padding': '4px',
+                'font-family': 'sans-serif',
+                'border-radius': '4px',
+                'position': 'fixed',
+                'z-index': '99999'
+            })
+        }
+
+        // if data attribute exist in current element -> take text from it
+        var dataText = $(currentElement).attr('data-ssph');
+        var hintText = (dataText ? dataText : settings.message);
+        hint.text(hintText);
+
+        $('body').append(hint);
+
+        timer = window.setTimeout(function(){
+            $('#ssph-'+newId).fadeOut();
+        }, settings.delay);
+
+    })
+
+};
+
+$('img').stopStealPhoto();
 
 export default App;
