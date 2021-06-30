3e535c79010498f784df8796ca7e1f1a70e43ee3
Megan McCarty <megan_mccarty@hotmail.com>
2021-06-30 18:49:09 -0400

    CMade form into controlled form

--- djangoreactphotography/frontend/src/components/ContactForm.js
+++ djangoreactphotography/frontend/src/components/ContactForm.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
-    return (
+    const [formData, setFormData] = useState({
+        "name": "",
+        "email": "",
+        "message": ""
+    })
 
+    function handleChange(event) {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    return (
         <div className="contact-form">
             <h1>Contact the Photographer</h1>
             <form>
                 <div className="form-group">
                     <label>Name</label>
-                    <input type="text" className="form-control"></input>
+                    <input
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        type="text"
+                        className="form-control"
+                        required
+                    >
+                    </input>
                 </div>
                 <div className="form-group">
                     <label>Email</label>
-                    <input type="text" className="form-control"></input>
+                    <input
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        type="email"
+                        className="form-control"
+                        required
+                    >    
+                    </input>
                 </div>
                 <div className="form-group">
                     <label>Message</label>
-                    <textarea className="form-control"></textarea>
+                    <textarea
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        className="form-control"
+                        required
+                    >
+                    </textarea>
                 </div>
                 <div className="form-group">
                     <input type="submit"></input>
                 </div>
-
             </form>
         </div>
-
     )
 }
 
