a60b0c84a7bda676c7410da43c2d66d5ca5e25c3
Megan McCarty <megan_mccarty@hotmail.com>
2021-07-01 18:16:52 -0400

    Sorted subcategories, filter orenders conditionally

--- djangoreactphotography/frontend/src/components/PhotoFilter.js
+++ djangoreactphotography/frontend/src/components/PhotoFilter.js
@@ -8,16 +8,18 @@ function PhotoFilter({ filter, onFilter, photos }) {
         }
     })
 
-    const subcategoriesToDisplay = [...subcategories].sort((a, b) => a > b ? 1 : -1)
-    
+    const subcategoriesToDisplay = [...subcategories].sort((a, b) => a > b ? 1 : -1);
+
     return (
-        <div className="filter">
-            <label>Filter by Subcategory</label>
-            <select className="form-control" value={filter} onChange={(e) => onFilter(e.target.value)}>
-                <option>All</option>
-                {subcategoriesToDisplay.map(subcategory => <option key={subcategory}>{subcategory[0].toUpperCase() + subcategory.slice(1)}</option>)}
-            </select>
-        </div>
+        subcategories.length > 1 ?
+            <div className="filter">
+                <label>Filter by Subcategory</label>
+                <select className="form-control" value={filter} onChange={(e) => onFilter(e.target.value)}>
+                    <option>All</option>
+                    {subcategoriesToDisplay.map(subcategory => <option key={subcategory}>{subcategory[0].toUpperCase() + subcategory.slice(1)}</option>)}
+                </select>
+            </div>
+            : null
     )
 }
 
