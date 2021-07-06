0dd0a6dbad7b0b51ececaa43f7f58bcfbad6f4a5
Megan McCarty <megan_mccarty@hotmail.com>
2021-07-06 12:27:28 -0400

    Fixed capitalization on green buttons

--- djangoreactphotography/frontend/src/components/HomeGalleryImage.js
+++ djangoreactphotography/frontend/src/components/HomeGalleryImage.js
@@ -4,6 +4,14 @@ import { Link } from 'react-router-dom';
 function HomeGalleryImage({ category, url, image, alt }) {
     const [isZoomed, setIsZoomed] = useState(false);
 
+    const newCategory = category.split(" ").map(word => {
+        if (word !== "and") {
+            return word[0].toUpperCase() + word.slice(1);
+        } else {
+            return word;
+        }
+    }).join(" ");
+
     function handleZoomIn() {
         setIsZoomed(true);
     }
@@ -20,8 +28,8 @@ function HomeGalleryImage({ category, url, image, alt }) {
             </div>
 
             <Link to={url}>
-                <h2 onMouseEnter={handleZoomIn} onMouseLeave={handleZoomOut} className="gallery-button green">
-                    {category}
+                <h2 onMouseEnter={handleZoomIn} onMouseLeave={handleZoomOut} className="green">
+                    {newCategory}
                 </h2>
             </Link>
         </div>
